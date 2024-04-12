284ee3f18d49380ad30f4ea9835dfea70ec74a5c
michener46 <52713659+michener46@users.noreply.github.com>
2024-04-12 07:20:01 -0400

    Trained new model

--- ChessEvaluator.py
+++ ChessEvaluator.py
@@ -2,7 +2,6 @@ import csv
 import os
 import json
 import numpy as np
-import pandas as pd
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense
@@ -17,11 +16,15 @@ target_data = []
 
 file_path = 'lichess_db_eval_modified.jsonl'
 filename = 'testing.csv'
+i = 0
 print(f"Attempting to load data from {file_path}")
 with open(file_path, 'r', encoding='utf-8') as file:
     for line in file:
         data = json.loads(line)
         if 'board_position' in data and 'evals' in data and data['evals'][0]['pvs']:
+            i += 1
+            if i % 100000 == 0:
+                print(i)
             # Assuming we're interested in the 'cp' value of the first 'pvs' of the first 'evals'
             input_data.append(data['board_position'])
             if 'cp' in data["evals"][0]["pvs"][0]:
@@ -43,15 +46,22 @@ if os.path.exists(model_path):
 else:
     print("Creating new model...")
     model = Sequential([
-        Dense(1024, activation='relu', input_shape=(64,)),
-        Dense(1024, activation='relu'),
-        Dense(1024, activation='relu'),
+        Dense(64, activation='relu', input_shape=(64,)),
+        Dense(64, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(64, activation='relu'),
         Dense(1)  # Output layer: Predicting a single value
     ])
     model.compile(optimizer='adam', loss='mean_squared_error')
 
 # Step 4: Train the Model
-model.fit(X_train, y_train, batch_size = 10000, epochs=100, validation_split=0.1)
+model.fit(X_train, y_train, batch_size = 4000, epochs=100, validation_split=0.1)
 
 # Step 5: Evaluate the Model
 test_loss = model.evaluate(X_test, y_test)
